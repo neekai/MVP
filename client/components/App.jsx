25d258fb7b29f46f4be453f29263c2a25f42b737
Hack Reactor Student <student@hackreactor.com>
2018-05-25 13:40:45 -0700

    getting the data from insults but trying to fix reqbody

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 
 class App extends React.Component {
     constructor(props) {
@@ -7,11 +9,22 @@ class App extends React.Component {
         this.state = {
             insult: ''
         }
+        this.handleInputAndSubmit = this.handleInputAndSubmit.bind(this);
     }
 
+    handleInputAndSubmit(e) {
+        e.preventDefault();
+        axios.get('/api/punch')
+          .then(response => {
+            this.setState({
+            insult: response.data
+          })
+      })
+          .catch(error => {throw error; console.log('There was an error getting insult back from punchLineController')})
+        e.target.reset();
+  }
 
-
-
+ 
 
 
 
@@ -22,7 +35,15 @@ class App extends React.Component {
 
     render() {
         return (
-            
+            <div>
+                <h1>I HATE YOU.</h1>
+                <form onClick= {e => this.handleInputAndSubmit(e)} >
+                    I HATE YOU: <input type="text"/>
+                    <button> Screw You! </button>
+                </form>
+            </div>
         )
     }
 }
+
+export default App;
--- server/controllers/punchLineController.js
+++ server/controllers/punchLineController.js
@@ -1,7 +1,9 @@
 const request = require('request');
+const express = require('express');
 
 const punchLineController = {
     FETCH: (req, res) => {
+        console.log('this is the reqbody..', req.body);
         let name = req.body.name;
         request('https://insult.mattbas.org/api/insult.txt?who=' + name, function(error, response, body) {
             console.log('error: ', error);
@@ -9,7 +11,7 @@ const punchLineController = {
             console.log('body: ', body);
         })
         .then(data => {res.status(200).send(data); console.log('successfully fetched insult from punchLineController..', data)})
-        .catch(err => {throw err; res.status(404); console.log('Could not fetch insult from punchLineController..', err)});
+        .catch(err => {throw err; res.status(404); console.log('Could not fetch insult from insult api..', err)});
     }
 }
 
